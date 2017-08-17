d4174e7c97eaba7f6995b259897dcc1308791579
ram-ganesh-16 <30903745+ram-ganesh-16@users.noreply.github.com>
2017-08-17 20:30:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,17 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article one', function (req, res) {
+  res.sendFile('ur big asshole');
+});
+
+app.get('/article two', function (req, res) {
+  res.sendFile('go fuck urself');
+});
+
+app.get('/article three', function (req, res) {
+  res.sendFile('come suck my dick');
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
