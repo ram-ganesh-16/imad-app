5121d667e3b87e4bb8402c6f8a9ec040b9f4dc52
ram-ganesh-16 <30903745+ram-ganesh-16@users.noreply.github.com>
2017-08-20 14:48:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,13 +1,1 @@
-console.log('Loaded!');
-
-var img =document.getElementById('madi');
-
-var marginLeft=0;
-function moveRight(){
-    marginLeft=marginLeft+ 1;
-    img.style.marginLeft=marginLeft + 'px';
-}
-img.onclick= function(){
-    
-    var interval = setInterval(moveRight,50) ;
-};
+s
