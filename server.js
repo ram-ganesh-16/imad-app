03c8085456a82db6a1c2d952684f6713e73b83a6
ram-ganesh-16 <30903745+ram-ganesh-16@users.noreply.github.com>
2017-08-20 10:40:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -115,7 +115,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/articles', function (req, res) {
+app.get('/personal', function (req, res) {
   res.send((createtemplate(personal)));
 });
 
