fdb2afc052bd7f1f25df88aa09254acc29b44803
ram-ganesh-16 <30903745+ram-ganesh-16@users.noreply.github.com>
2017-08-20 14:52:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -109,7 +109,7 @@ app.get('/', function (req, res) {
 });
 var counter=0;
 app.get('/counter',function(req, res) {
-    counter=counter+1000000000;
+    counter=counter+1;
     res.send(counter.toString());
 });
 app.get('/personal', function (req, res) {
