a1eb3e5a24d9f0d6d89d698b8882d01e307fd467
ram-ganesh-16 <30903745+ram-ganesh-16@users.noreply.github.com>
2017-08-20 17:05:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,23 @@
 var button = document.getElementById('counter');
  
+ button.onclick = function () { 
+  var request = new XMLHttpRequest();
+  request.onreadystatechange=function(){
+      if (request.readyState ===XMLHttpRequest.DONE) {
+          if (request.status ===200){
+              var counter = request.responseText;
+              var span = document.getElementById('count');
+              span.innerHTML= counter.toString();
+              
+          }
+          
+      }
+  };
+  request.open('GET','http://ganeshganni111.imad.hasura-app.io/counter',true);
+  request.send(null);
+
+  
+};
 
 var submit=document.getElementById('submit_button');
 submit.onclick=function(){
