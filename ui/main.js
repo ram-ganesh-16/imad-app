59c0441f486fa40c1330d0426a919fc11dfcbeb7
ram-ganesh-16 <30903745+ram-ganesh-16@users.noreply.github.com>
2017-08-20 13:34:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,6 +3,6 @@ console.log('Loaded!');
 var element=document.getElementById('main-text');
 element.innerHTML='new value';
 var img =document.getElementById('img');
-img.onclick=function(){
+madi.onclick= function(){
     img.style.marginleft = '100px' ;
 };
