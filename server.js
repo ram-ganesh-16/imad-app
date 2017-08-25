5305ae9a1d300029656666c0bee85cf2969d8b99
ram-ganesh-16 <30903745+ram-ganesh-16@users.noreply.github.com>
2017-08-25 11:21:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var pool=require('pg').pool;
 
+var config = {
+    user:'ganeshganni111',
+    database:'ganeshganni111',
+    host:'db.imad.hasura.imad.io',
+    port:'5432',
+    password: process.env.DB_PASSWORD
+};
 var app = express();
 app.use(morgan('combined'));
 
@@ -111,6 +119,17 @@ var counter=0;
 app.get('/counter',function(req, res) {
     counter=counter+1;
     res.send(counter.toString());
+});
+
+var pool=new pool(config);
+app.get('/test-db',function(req,res){
+    pool.query('select * FROM test',function(err,result){
+       if(err){
+           res.status (500).send(err.toString());
+       } else {
+           res.send(JSON.stringify(result));
+       }
+    });
 });
 
 app.get('/ui/main.js', function (req, res) {
