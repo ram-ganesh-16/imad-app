5e8816cdc90ac9e880306438197bb8f856766d14
ram-ganesh-16 <30903745+ram-ganesh-16@users.noreply.github.com>
2017-08-25 11:42:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -123,7 +123,7 @@ app.get('/counter',function(req, res) {
 
 var pool=new Pool(config);
 app.get('/test-db',function(req,res){
-    pool.query('Select * FROM test',function(err,result){
+    pool.query('SELECT * FROM test',function(err,result){
        if(err){
            res.status (500).send(err.toString());
        } else {
